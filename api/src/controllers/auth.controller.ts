1b979408ca6915fd7ff5a24b977daad9e19e66a2
Abba Sali Aboubakar Mamate <abbasalimokolo@gmail.com>
2023-06-23 09:00:05 +0100

    add next method on controller

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { CreateUserInput, LoginUserInput } from "../shared/types/models";
 import * as bcrypt from "bcryptjs";
 import authService from "../domain/services/auth.service";
@@ -6,7 +6,11 @@ import { TokenPayload } from "../shared/types/common";
 import * as jwt from "jsonwebtoken";
 import { EXPIRES, JWT_SECRET } from "../shared/core/config";
 
-const registerController = async (req: Request, res: Response) => {
+const registerController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { email, fullname, userType }: CreateUserInput = req.body;
   const password: string = bcrypt.hashSync(req.body.password, 10);
   const createUser = await authService.registerService({
@@ -20,7 +24,11 @@ const registerController = async (req: Request, res: Response) => {
   });
 };
 
-const loginController = async (req: Request, res: Response) => {
+const loginController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { email, password }: LoginUserInput = req.body;
   const user = await authService.findByEmail(email);
   if (!user) {
@@ -41,4 +49,3 @@ const loginController = async (req: Request, res: Response) => {
 };
 
 export { registerController, loginController };
-
--- api/src/controllers/wantedPoster.controller.ts
+++ api/src/controllers/wantedPoster.controller.ts
@@ -1,11 +1,15 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { UserModel } from "../domain/models/user.model";
 import { WantedPosterUpdateParams } from "../domain/models/wantedPoster.model";
 import wantedPosterService from "../domain/services/wantedPoster.service";
 import { CreateWantedPosterInput } from "../shared/types/models";
 import { parseRequest } from "../utils/helpers";
 
-const createWantedPosterController = async (req: Request, res: Response) => {
+const createWantedPosterController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const {
     description,
     phone,
@@ -13,7 +17,7 @@ const createWantedPosterController = async (req: Request, res: Response) => {
     find,
     date,
     commentaire,
-    city
+    city,
   }: CreateWantedPosterInput = req.body;
   const user = await UserModel.findById({ _id: req.body.user });
   const createWantedPoster =
@@ -25,33 +29,49 @@ const createWantedPosterController = async (req: Request, res: Response) => {
       user,
       date,
       commentaire,
-      city
+      city,
     });
   await user?.save();
   await createWantedPoster.save();
-  return res.status(200).json({ wantedPoster: createWantedPoster });
+  res.status(200).json({ wantedPoster: createWantedPoster });
+  next();
 };
 
-const getWantedPosterController = async (req: any, res: Response) => {
+const getWantedPosterController = async (
+  req: any,
+  res: Response,
+  next: NextFunction
+) => {
   const wantedPosters = await wantedPosterService.getAllWantedPosterService(
     req?.user?.id,
     "user"
   );
-  return res.status(200).json({ wantedPosters });
+  res.status(200).json({ wantedPosters });
+  next();
 };
-const findWantedPosterController = async (req: Request, res: Response) => {
+const findWantedPosterController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const wantedPosters = await wantedPosterService.findWantedPosterService();
-  return res.status(200).json({ wantedPosters });
+  res.status(200).json({ wantedPosters });
+  next();
 };
 
-const updateWantedPosterController = async (req: Request, res: Response) => {
+const updateWantedPosterController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const id = req.params.id;
   const data = parseRequest(req.body, WantedPosterUpdateParams);
   const updateWanted = await wantedPosterService.updateWantedPosterService(
     id,
     data
   );
-  return res.json(updateWanted);
+  res.json(updateWanted);
+  next();
 };
 
 export {
